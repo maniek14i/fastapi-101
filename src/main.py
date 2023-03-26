42dfe8cac85401bb1c03724dc2ffba9f17af5312
maniek14i <mariuszgrzesiak0@gmail.com>
2023-03-26 16:34:34 +0200

    change

--- src/main.py
+++ src/main.py
@@ -112,7 +112,10 @@ def delete_post(
 
     return post
 
+@app.get('/')
+def get_main():
+    return ( 200: "OK" )
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=80, reload=True)
